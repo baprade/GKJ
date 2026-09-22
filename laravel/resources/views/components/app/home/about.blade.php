62cac1bf23cc58815a6b27ca2a344ca6cfa4cdd4
Bagas Prasta Dewa <orionnebulabpd@gmail.com>
2026-09-22 16:08:16 +0700

    fix(ui): enforce bright white text and shadow for about section cinzel h1

--- laravel/resources/views/components/app/home/about.blade.php
+++ laravel/resources/views/components/app/home/about.blade.php
@@ -1,6 +1,6 @@
 <div style="background: url({{ route('images', config('app.image')) }}); background-size: contain; background-position: center;">
     <div class="flex flex-col items-center justify-center w-full gap-4 px-8 py-6 text-white sm:py-10 sm:px-14 bg-black/60">
-        <h1 class="text-xl font-semibold text-center sm:text-2xl cinzel">Gereja Kristen Jawa (GKJ) Wonogiri</h1>
-        <div class="text-sm text-center sm:text-base text-shadow-10">Adalah salah satu gereja yang melayani umat Kristen di Kabupaten Wonogiri, Jawa Tengah. Gereja ini berdiri sebagai tempat ibadah sekaligus pusat kegiatan rohani dan sosial bagi jemaatnya. Dengan memadukan nilai-nilai kekristenan dan budaya Jawa, GKJ Wonogiri menciptakan suasana yang penuh kehangatan dan kebersamaan. Beragam kegiatan, mulai dari ibadah rutin hingga pelayanan sosial, diadakan untuk menjangkau semua kalangan jemaat. Komitmen gereja ini adalah menjadi terang dan berkat bagi masyarakat di sekitarnya.</div>
+        <h1 class="text-xl font-semibold text-center text-white sm:text-2xl cinzel text-shadow-10">Gereja Kristen Jawa (GKJ) Wonogiri</h1>
+        <div class="text-sm text-center text-white sm:text-base text-shadow-10">Adalah salah satu gereja yang melayani umat Kristen di Kabupaten Wonogiri, Jawa Tengah. Gereja ini berdiri sebagai tempat ibadah sekaligus pusat kegiatan rohani dan sosial bagi jemaatnya. Dengan memadukan nilai-nilai kekristenan dan budaya Jawa, GKJ Wonogiri menciptakan suasana yang penuh kehangatan dan kebersamaan. Beragam kegiatan, mulai dari ibadah rutin hingga pelayanan sosial, diadakan untuk menjangkau semua kalangan jemaat. Komitmen gereja ini adalah menjadi terang dan berkat bagi masyarakat di sekitarnya.</div>
     </div>
 </div>
