fe2b62ef61e885bc442cdd1e6afcd293a6af8098
Bagas Prasta Dewa <orionnebulabpd@gmail.com>
2026-09-22 14:25:32 +0700

    perf(seo): optimize homepage meta title and description for GKJ search terms

--- laravel/resources/views/livewire/app/home.blade.php
+++ laravel/resources/views/livewire/app/home.blade.php
@@ -1,3 +1,5 @@
+@section('page_title', 'GKJ Wonogiri - Gereja Kristen Jawa Wonogiri | Jadwal Ibadah & Warta Jemaat')
+@section('page_description', 'Website resmi Gereja Kristen Jawa (GKJ) Wonogiri. Informasi jadwal ibadah Minggu Gedung Induk dan Pepanthan, renungan harian, warta jemaat, dan streaming ibadah.')
 <div class="grow">
     <x-app.home.hero :homeslide="$homeslide"/>
     <x-app.home.about/>
